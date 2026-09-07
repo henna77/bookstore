4d73d6074b8abef80b5712ea8ec335db4ec77e34
henna77 <henna.mikkonen07@gmail.com>
2026-09-08 02:02:55 +0300

    Bookstore project

---
+++ src/main/java/syksy26/bookstore/web/BookController.java
@@ -0,0 +1,15 @@
+package syksy26.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BookController {
+
+    @GetMapping("/bookstore")
+    @ResponseBody
+    public String index() {
+        return "Bookstore";
+    }
+}
